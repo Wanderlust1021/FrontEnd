19ce5aa0945003b2238fcb10bd70c4ac60825593
Ty Lippe <tylippe94@gmail.com>
2020-01-10 16:49:35 -0500

    work

--- wanderlust/src/state/Reducers.js
+++ wanderlust/src/state/Reducers.js
@@ -1,9 +1,7 @@
 import * as types from "./Actions";
 
 const initialAppState = {
-  experiences: [],
-  organizers: [],
-  data: [],
+  appState: [],
   isLoading: false,
   error: null
 };
@@ -20,26 +18,13 @@ export function experienceReducer(state = initialAppState, action) {
       return {
         ...state,
         isLoading: false,
-        data: action.payload
+        appState: action.payload
       };
     case types.FETCH_FAILED:
       return {
         ...state,
         error: action.payload
       };
-
-    case types.ADD_EXPERIENCE:
-      debugger
-      return {
-        ...state,
-        data: [...state.data, action.payload]
-      }
-
-    case types.FILTER_EXPERIENCES:
-      return {
-        ...state,
-        data: action.payload
-      }
 
     default:
       return state;
@@ -49,44 +34,42 @@ export function experienceReducer(state = initialAppState, action) {
 const initialAuthState = {
   isAuthenticated: false,
   error: null,
-  username: '',
-  orgname: '',
+  username: "",
   isLoading: false
 };
 
-export const authReducer = (state=initialAuthState, action) => {
-    switch(action.type) {
-      case types.AUTH_START:
-        return {
-          ...state,
-          isLoading: true
-        }
+export const authReducer = (state = initialAuthState, action) => {
+  switch (action.type) {
+    case types.AUTH_START:
+      return {
+        ...state,
+        isLoading: true
+      };
 
-      case types.LOGIN_SUCCESS:
-        return {
-          ...state,
-          isAuthenticated: true,
-          isLoading: false,
-          username: action.payload.username,
-          orgname: action.payload.organization
-        }
-  
-      case types.SIGNUP_SUCCESS:
-        return {
-          ...state,
-          username: action.payload,
-          isLoading: false
-        }
-  
-      case types.AUTH_FAILURE:
-        debugger
-        return {
-          isAuthenticated: false,
-          error: action.payload,
-          isLoading: false
-        }
+    case types.LOGIN_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        username: action.payload
+      };
 
-      default:
-        return state;
-    }
+    case types.SIGNUP_SUCCESS:
+      return {
+        ...state,
+        username: action.payload,
+        isLoading: false
+      };
+
+    case types.AUTH_FAILURE:
+      debugger;
+      return {
+        isAuthenticated: false,
+        error: action.payload,
+        isLoading: false
+      };
+
+    default:
+      return state;
   }
+};
